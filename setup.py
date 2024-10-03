bceb1f6dd6df85d21207caf59674e1f4c83fc942
Justin Johnson <jajohns@umn.edu>
2024-10-03 09:08:28 -0500

    modified setup.py to work with editable installs

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from setuptools import setup, find_packages
+import numpy
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+import os
 
 packages=find_packages()
 include_package_data=True
+
+extensions = [
+        Extension(
+        "hazelbean.calculation_core.cython_functions",  # This corresponds to the Python import path
+        ["hazelbean/calculation_core/cython_functions.pyx"],  # Path to the .pyx file
+        
+    ),
+        Extension("hazelbean.calculation_core.aspect_ratio_array_functions", ["hazelbean/calculation_core/aspect_ratio_array_functions.pyx"]),
+]
+
+
 
 setup(
   name = 'hazelbean',
@@ -15,7 +30,10 @@ setup(
   # download_url = 'https://github.com/jandrewjohnson/hazelbean/releases/hazelbean_x64_py3.6.3/dist/hazelbean-0.3.0_x64_py3.6.3.tar.gz',
   keywords = ['geospatial', 'raster', 'shapefile'],
   classifiers = [],
-  ext_modules=[Extension("cython_functions", ["cython_functions.c"]),
-               Extension("aspect_ratio_array_functions", ["aspect_ratio_array_functions.c"]),
-               ]
+  ext_modules=cythonize(extensions),
+  # ext_modules=[Extension("cython_functions", ["cython_functions.c"]),
+  #              Extension("aspect_ratio_array_functions", ["aspect_ratio_array_functions.c"]),
+  #              ],
+  include_dirs=[numpy.get_include()],
+  cmdclass={'build_ext': build_ext},
 )
