bd23c449cea4a173eb575ea2c84eff5a139a2de8
Chiara Colesanti Senni <chiacole@hotmail.it>
2025-12-08 22:02:45 +0100

    Minor bugs fixed in multiplication and division funtions

--- hazelbean/arrayframe.py
+++ hazelbean/arrayframe.py
@@ -294,7 +294,7 @@ class ArrayFrame(object):
         raise NameError('Cannot directly set nonzero_mask. Use set_nonzero_mask() method.')
 
     def set_nonzero_mask(self):
-        self._nonzero_mask = np.where(self.data == 1, 1, 0).astype(np.ubyte)
+        self._nonzero_mask = np.where(self.data != 0, 1, 0).astype(np.ubyte)
         self.num_nonzero = np.count_nonzero(self._nonzero_mask)
         self.num_zero = self.size - self.num_nonzero
         self.nonzero_mask_set = True
@@ -349,13 +349,13 @@ class ArrayFrame(object):
 
     def __mul__(self, after):
         def op(left, right):
-            return left + right
+            return left * right
         output_path = hb.temp(filename_start='mul', remove_at_exit=True)
         return hb.raster_calculator_flex([self.path, after.path], op, output_path)
 
     def __truediv__(self, after):
         def op(left, right):
-            return left + right
+            return np.where(right != 0, left / right, 0)
         output_path = hb.temp(filename_start='div', remove_at_exit=True)
         return hb.raster_calculator_flex([self.path, after.path], op, output_path)
 
